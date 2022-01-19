7ab12efdbdbf0efb4a7c10e80a1423da82fabb8c
keka3avr <kekazavr228@gmail.com>
2022-01-20 00:45:15 +1100

    styled-components removed

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
 import App from './app/App';
 import { store } from './app/redux/store';
-import { theme } from './app/theme';
 
 ReactDOM.render(
     <Provider store={store}>
-        <ThemeProvider theme={theme}>
-            <App />
-        </ThemeProvider>
+        <App />
     </Provider>,
 
     document.getElementById('root'),
